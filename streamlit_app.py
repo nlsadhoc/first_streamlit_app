6d7bb2be28774f02cc7deb91b909b64e86f3f4b6
nlsadhoc <137865880+nlsadhoc@users.noreply.github.com>
2023-06-27 12:35:36 +0530

    Update streamlit_app.py
    implemented pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 
 streamlit.text('🥣 🥗 🐔 🥑🍞 Scotch Eggs')
+
+import pandas
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
